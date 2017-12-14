555c40d49c3b8f933b82889de28cdb05a1d0b31f
homewangyl <313922692@qq.com>
2017-12-14 22:58:18 +0800

    xxx

---
+++ app/controllers/home.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Article = mongoose.model('Article');
+
+module.exports = (app) => {
+  app.use('/', router);
+};
+
+router.get('/', (req, res, next) => {
+  Article.find((err, articles) => {
+    if (err) return next(err);
+    res.render('blog/index', {
+      title: 'Generator-Express MVC',
+      articles: articles,
+      pretty:true
+    });
+  });
+});
